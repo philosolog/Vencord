fe092333e519716d7bf035bd4be809ba47bdb156
philosolog <philosolog@gmail.com>
2026-05-23 05:25:54 -0700

    Add Dev tag

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -638,6 +638,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    philosolog: {
+        name: "philosolog",
+        id: 897728576659529780n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
